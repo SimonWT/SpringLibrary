583de40724796cbb518e5bcc9c50d112bcc580e3
Igor <i.vahula@innopolis.ru>
2018-04-01 17:40:06 +0300

    Queue Implementation (If number of copies equals zero -> queue, otherwise -> history) and type form (registration page)

---
+++ src/main/java/net/proselyte/springsecurityapp/dao/QueueDao.java
@@ -0,0 +1,13 @@
+package net.proselyte.springsecurityapp.dao;
+
+import net.proselyte.springsecurityapp.model.Booking.Queue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QueueDao extends JpaRepository<Queue, Long> {
+
+    Queue getQueueById(Long id);
+
+    List<Queue> findAllByUserId(Long id);
+}
